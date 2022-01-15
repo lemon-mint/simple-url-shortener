1332cfee57ec40530731c9285d0383eab06f98a0
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-15 21:23:18 +0900

    envaddr, godotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lemon-mint/simple-url-shortener
 
 go 1.17
+
+require (
+	github.com/lemon-mint/envaddr v0.0.0-20220115112619-b1acf77f317e
+	github.com/lemon-mint/godotenv v1.0.3
+)
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/lemon-mint/envaddr"
+	"github.com/lemon-mint/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	lnHost := envaddr.Get(":9090")
+	log.Println("Listening on", lnHost)
 }
